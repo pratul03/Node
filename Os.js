518d4721a91cbc2e4411c9bbe4adc15aa95c3724
Pratul Makar <pratulmakar02@gmail.com>
2024-08-03 13:55:17 +0530

    new

---
+++ Os.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ * 4. Write a Node.js program that uses the OS module to retrieve information about the current operating system.
+ */
+
+const os = require('node:os');
+console.log(os.platform());
